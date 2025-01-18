c0a0cc166011383da3696858e49e0aa36ecfb7b6
poipiii <nanosonico1@gmail.com>
2025-01-18 23:30:10 +0800

    [feat] implement report voting system and enhance user report management with middleware and OpenAPI routes

--- backend/db/schema.ts
+++ backend/db/schema.ts
@@ -1,10 +1,10 @@
-import { int, sqliteTable, text ,} from "drizzle-orm/sqlite-core";
+import { int, sqliteTable, text, uniqueIndex ,} from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 
 export const users = sqliteTable("users", {
   id: int().primaryKey({ autoIncrement: true }),
   username: text().notNull(),
-  pkey: int().notNull(),
+  pkey: text().notNull(),
   reputation : int(),
 });
 
@@ -13,8 +13,21 @@ export const users = sqliteTable("users", {
 
 export const reports = sqliteTable("reports", {
   id: int().primaryKey({ autoIncrement: true }),
-  user_id: int("user_id").references(() => users.id),
+  user_id: int("user_id").references(() => users.id, { onDelete: 'cascade' }),
+  report_hash: text("report_hash").notNull(),
   report_text: text("report_text").notNull(),
+  created_at: int({mode: "timestamp_ms"}).default(sql`(CURRENT_TIMESTAMP)`),
+}, (table) => ({
+  report_hash_idx: uniqueIndex("report_hash_idx").on(table.report_hash),
+}));
+
+
+export const report_votes = sqliteTable("report_votes", {
+  id: int().primaryKey({ autoIncrement: true }),
+  report_id: int("report_id").references(() => reports.id, { onDelete: 'cascade' }),
+  user_id: int("user_id").references(() => users.id, { onDelete: 'cascade' }),
+  upvote: int("upvote").notNull(),
+  downvote: int("downvote").notNull(),
   created_at: int({mode: "timestamp_ms"}).default(sql`(CURRENT_TIMESTAMP)`),
 });
 
